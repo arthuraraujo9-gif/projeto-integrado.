ad135e7ff72de4dc59597a94e49a077b2f7ca025
Aluno <aluno.formoso@ifto.local>
2026-06-02 12:09:47 -0300

    subindo arquivos POO

--- POO/carro.py
+++ POO/carro.py
@@ -1,5 +1,8 @@
 # A palavra "class" é usada para criar uma classe.
 # Uma classe funciona como um molde para criar objetos
+from functools import reduce
+
+
 class Carro:
 
     # Método Construtor
@@ -7,15 +10,32 @@ class Carro:
         self.marca = marca
         self.modelo = modelo
         self.ano = ano
-        self.velocidade = velocidade
+        self.velocidade = velocity = velocidade
 
     # Métodos
     # Método acelerar
+    # "aumento" sera o valor recebido para aumenta a velocidade.
     def acelerar(self, aumento):
-        # self.velocidade = self.velocidade + aumento 
+        # self.velocidade = self.velocidade + reducao
         self.velocidade += aumento
 
-        print(f"O carro acelerou para{self.velocidade} km/h")
+        print(f"O carro acelerou para {self.velocidade} km/h")
+
+    # método frear
+    def frear(self, reducao):
+        # self.velocidade = self.velocidade - reducao
+        self.velocidade -= reducao
+        print(f"O carro freou para {self.velocidade} km/h")
+
+    # metodo para exibir informações
+    def exibir_info(self):
+        print("=== informações do carro ===")
+
+        # exibe os atributos do objeto
+        print(f"Marca: {self.marca}")
+        print(f"Modelo: {self.modelo}")
+        print(f"Ano: {self.ano}")
+        print(f"velocidade atual: {self.velocidade}")
 
 
 # Criando um objeto da Classe Carro
@@ -23,18 +43,28 @@ class Carro:
 # "carro1" é uma variável que recebe um objeto
 carro1 = Carro("Chevrolet", "S10", 2013)
 
-# Exibir informações do carro 1
-print(f"Marca: {carro1.marca}")
-print(f"Modelo: {carro1.modelo}")
-print(f"Ano: {carro1.ano}")
 
+# chamando os metodos
+
+# o valor 50 enviado para o paramento "aumento"
+# carro1.reducao
 carro1.acelerar(50)
+
+# o valor 20 sera enviado para o paramento "reducao"
+carro1.frear(20)
+
+# exibindo as informações do carro
+carro1.exibir_info()
+
+
+
 
 # # "carro2" é uma variável que recebe um objeto
 carro2 = Carro("BYD", "Dolphin Mini", 2025)
 
-# # Exibir informações do carro 2
-print(f"Marca: {carro2.marca}")
-print(f"Modelo: {carro2.modelo}")
-print(f"Ano: {carro2.ano}")
+carro2.acelerar (50)
+
+carro2.frear(20)
+
+carro2.exibir_info()
 
